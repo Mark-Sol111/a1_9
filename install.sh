7d4292e513f727c61b1ef106e9b22ef6b691e387
Mark Sol <114338588+Mark-Sol111@users.noreply.github.com>
2022-09-28 23:35:17 +1100

    update gunicorn config & install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-alias l="ls -ln"
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+alias l="ln -la"
+alias gr='sudo /etc/init.d/gunicorn restart' # restart kunicorn
+alias nr='sudo /etc/init.d/nginx restart'
+
+sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/wsgi
 sudo ln -s /home/box/etc/nginx-proxy.conf /etc/nginx/sites-enabled/nginx-proxy.conf
-sudo rm /etc/nginx/sites-enabled/deffault.conf
+sudo rm /etc/nginx/sites-enabled/default
+gr
+nr
